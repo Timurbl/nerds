68b87bcd3be901e6f0743241d2089da45bf4678b
Timurbl <32030037+Timurbl@users.noreply.github.com>
2018-12-15 19:30:45 +0300

    Стилизация шаблонов в магазине
    Стили и js к ним

---
+++ templates.js
@@ -0,0 +1,76 @@
+var templates = document.querySelectorAll(".templates__item");
+
+
+templates[0].addEventListener("mouseover", function (evt) {
+    evt.preventDefault();
+    var block = templates[0].querySelector('.item__description');
+    block.classList.remove('visually-hidden')
+});
+
+templates[1].addEventListener("mouseover", function (evt) {
+    evt.preventDefault();
+    var block = templates[1].querySelector('.item__description');
+    block.classList.remove('visually-hidden')
+});
+
+templates[2].addEventListener("mouseover", function (evt) {
+    evt.preventDefault();
+    var block = templates[2].querySelector('.item__description');
+    block.classList.remove('visually-hidden')
+});
+
+templates[3].addEventListener("mouseover", function (evt) {
+    evt.preventDefault();
+    var block = templates[3].querySelector('.item__description');
+    block.classList.remove('visually-hidden')
+});
+
+templates[4].addEventListener("mouseover", function (evt) {
+    evt.preventDefault();
+    var block = templates[4].querySelector('.item__description');
+    block.classList.remove('visually-hidden')
+});
+
+templates[5].addEventListener("mouseover", function (evt) {
+    evt.preventDefault();
+    var block = templates[5].querySelector('.item__description');
+    block.classList.remove('visually-hidden')
+});
+
+
+
+templates[0].addEventListener("mouseout", function (evt) {
+    evt.preventDefault();
+    var block = templates[0].querySelector('.item__description');
+    block.classList.add('visually-hidden')
+});
+
+templates[1].addEventListener("mouseout", function (evt) {
+    evt.preventDefault();
+    var block = templates[1].querySelector('.item__description');
+    block.classList.add('visually-hidden')
+});
+
+templates[2].addEventListener("mouseout", function (evt) {
+    evt.preventDefault();
+    var block = templates[2].querySelector('.item__description');
+    block.classList.add('visually-hidden')
+});
+
+templates[3].addEventListener("mouseout", function (evt) {
+    evt.preventDefault();
+    var block = templates[3].querySelector('.item__description');
+    block.classList.add('visually-hidden')
+});
+
+templates[4].addEventListener("mouseout", function (evt) {
+    evt.preventDefault();
+    var block = templates[4].querySelector('.item__description');
+    block.classList.add('visually-hidden')
+});
+
+templates[5].addEventListener("mouseout", function (evt) {
+    evt.preventDefault();
+    var block = templates[5].querySelector('.item__description');
+    block.classList.add('visually-hidden')
+});
