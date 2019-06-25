80c1460c1cff57f862e8e13eb00e7adda875b65e
Timurbl <32030037+Timurbl@users.noreply.github.com>
2019-06-25 20:28:06 +0300

    listener rewrite

--- templates.js
+++ templates.js
@@ -23,14 +23,7 @@
             container.appendChild(element)
         });
 
-
         templatesClick();
-    }
-
-    function clearContainer(container) {
-        while (container.children.length != 0) {
-            container.removeChild(container.firstChild);
-        }
     }
 
     function getElementFromTemplate(data) {
@@ -52,132 +45,35 @@
         var item__description = document.querySelectorAll(".item__description");
         var path = document.querySelectorAll("path");
 
+        for (let i = 0; i < templates.length; i++) {
+            templatesClickListener(templates[i], item__description[i], path[i])
+        }
+    }
 
-        templates[0].addEventListener("mouseover", function (evt) {
+    function templatesClickListener(template, item, path) {
+        template.addEventListener("mouseover", function (evt) {
             evt.preventDefault();
-            var block = templates[0].querySelector('.item__description');
+            var block = template.querySelector('.item__description');
             block.classList.remove('visually-hidden')
         });
-
-        templates[1].addEventListener("mouseover", function (evt) {
+        template.addEventListener("mouseout", function (evt) {
             evt.preventDefault();
-            var block = templates[1].querySelector('.item__description');
-            block.classList.remove('visually-hidden')
-        });
-
-        templates[2].addEventListener("mouseover", function (evt) {
-            evt.preventDefault();
-            var block = templates[2].querySelector('.item__description');
-            block.classList.remove('visually-hidden')
-        });
-
-        templates[3].addEventListener("mouseover", function (evt) {
-            evt.preventDefault();
-            var block = templates[3].querySelector('.item__description');
-            block.classList.remove('visually-hidden')
-        });
-
-        templates[4].addEventListener("mouseover", function (evt) {
-            evt.preventDefault();
-            var block = templates[4].querySelector('.item__description');
-            block.classList.remove('visually-hidden')
-        });
-
-        templates[5].addEventListener("mouseover", function (evt) {
-            evt.preventDefault();
-            var block = templates[5].querySelector('.item__description');
-            block.classList.remove('visually-hidden')
-        });
-
-
-        templates[0].addEventListener("mouseout", function (evt) {
-            evt.preventDefault();
-            var block = templates[0].querySelector('.item__description');
+            var block = template.querySelector('.item__description');
             block.classList.add('visually-hidden')
         });
-
-        templates[1].addEventListener("mouseout", function (evt) {
+        item.addEventListener("mouseover", function (evt) {
             evt.preventDefault();
-            var block = templates[1].querySelector('.item__description');
-            block.classList.add('visually-hidden')
+            path.setAttribute("fill", "#4D4D4D");
         });
-
-        templates[2].addEventListener("mouseout", function (evt) {
+        item.addEventListener("mouseout", function (evt) {
             evt.preventDefault();
-            var block = templates[2].querySelector('.item__description');
-            block.classList.add('visually-hidden')
-        });
-
-        templates[3].addEventListener("mouseout", function (evt) {
-            evt.preventDefault();
-            var block = templates[3].querySelector('.item__description');
-            block.classList.add('visually-hidden')
-        });
-
-        templates[4].addEventListener("mouseout", function (evt) {
-            evt.preventDefault();
-            var block = templates[4].querySelector('.item__description');
-            block.classList.add('visually-hidden')
-        });
-
-        templates[5].addEventListener("mouseout", function (evt) {
-            evt.preventDefault();
-            var block = templates[5].querySelector('.item__description');
-            block.classList.add('visually-hidden')
-        });
-
-
-
-        item__description[0].addEventListener("mouseover", function (evt) {
-            evt.preventDefault();
-            path[0].setAttribute("fill", "#4D4D4D");
-        });
-        item__description[1].addEventListener("mouseover", function (evt) {
-            evt.preventDefault();
-            path[1].setAttribute("fill", "#4D4D4D");
-        });
-        item__description[2].addEventListener("mouseover", function (evt) {
-            evt.preventDefault();
-            path[2].setAttribute("fill", "#4D4D4D");
-        });
-        item__description[3].addEventListener("mouseover", function (evt) {
-            evt.preventDefault();
-            path[3].setAttribute("fill", "#4D4D4D");
-        });
-        item__description[4].addEventListener("mouseover", function (evt) {
-            evt.preventDefault();
-            path[4].setAttribute("fill", "#4D4D4D");
-        });
-        item__description[5].addEventListener("mouseover", function (evt) {
-            evt.preventDefault();
-            path[5].setAttribute("fill", "#4D4D4D");
-        });
-
-
-        item__description[0].addEventListener("mouseout", function (evt) {
-            evt.preventDefault();
-            path[0].setAttribute("fill", "#e9e9e9");
-        });
-        item__description[1].addEventListener("mouseout", function (evt) {
-            evt.preventDefault();
-            path[1].setAttribute("fill", "#e9e9e9");
-        });
-        item__description[2].addEventListener("mouseout", function (evt) {
-            evt.preventDefault();
-            path[2].setAttribute("fill", "#e9e9e9");
-        });
-        item__description[3].addEventListener("mouseout", function (evt) {
-            evt.preventDefault();
-            path[3].setAttribute("fill", "#e9e9e9");
-        });
-        item__description[4].addEventListener("mouseout", function (evt) {
-            evt.preventDefault();
-            path[4].setAttribute("fill", "#e9e9e9");
-        });
-        item__description[5].addEventListener("mouseout", function (evt) {
-            evt.preventDefault();
-            path[5].setAttribute("fill", "#e9e9e9");
+            path.setAttribute("fill", "#e9e9e9");
         });
     }
 
+    function clearContainer(container) {
+        while (container.children.length != 0) {
+            container.removeChild(container.firstChild);
+        }
+    }
 }());
